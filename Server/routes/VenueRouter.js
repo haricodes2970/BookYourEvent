3c0d819e9e1054ccd4e560a1b4f8d7ec50863db9
haricode2970 <srihariprasad7078@gmail.com>
2026-03-04 09:44:13 +0530

    fix: venue route fixed to REST standard

--- Server/routes/VenueRouter.js
+++ Server/routes/VenueRouter.js
@@ -4,8 +4,8 @@ const { createVenue, getAllVenues, getVenueById, deleteVenue, approveVenue } = r
 const { protect } = require('../middleware/authMiddleware');
 const { authorizeRoles } = require('../middleware/roleMiddleware');
 
-router.post('/create', protect, authorizeRoles('venueOwner'), createVenue);
-router.get('/all', protect, getAllVenues);
+router.post('/', protect, authorizeRoles('venueOwner'), createVenue);
+router.get('/', protect, getAllVenues);
 router.get('/:id', protect, getVenueById);
 router.delete('/:id', protect, authorizeRoles('venueOwner', 'admin'), deleteVenue);
 router.patch('/:id/approve', protect, authorizeRoles('admin'), approveVenue);
